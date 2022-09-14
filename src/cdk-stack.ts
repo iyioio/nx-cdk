0e37f1260c58024dfaa412c36922c68e386c6870
Scott Vance <scott@iyio.io>
2022-09-14 12:19:07 -0400

    - init

---
+++ src/cdk-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+// import * as ec2 from 'aws-cdk-lib/aws-ec2';
+// import * as efs from 'aws-cdk-lib/aws-efs';
+// import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+// import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+// import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
+import { Construct } from 'constructs';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this,'test',{
+        removalPolicy:cdk.RemovalPolicy.DESTROY
+    })
+
+  }
+}
